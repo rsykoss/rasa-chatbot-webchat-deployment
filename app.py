7ba922b350dc742c7df345c742658fa4c2723f05
Ko Seoyoon <rsykoss@gmail.com>
2019-07-02 15:41:53 +0800

    feat: include frontend template

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask  
+from flask import render_template
+
+# creates a Flask application, named app
+app = Flask(__name__)
+
+# a route to display our html page gotten from [react-chat-widget](https://github.com/mrbot-ai/rasa-webchat)
+@app.route("/")
+def index():  
+    return render_template('index.html')
+
+# run the application
+if __name__ == "__main__":  
+    app.run(debug=True)
